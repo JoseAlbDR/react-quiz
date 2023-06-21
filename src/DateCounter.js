e88ab3cc3c7bee2e5a9f695a778f9cb50bb51e86
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-21 15:57:40 +0200

    useReducer refactor

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -15,19 +17,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: "dec", payload: -1 });
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch({ type: "inc", payload: 1 });
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    dispatch(+e.target.value);
+    dispatch({ type: "setCount", payload: +e.target.value });
     // setCount(Number(e.target.value));
   };
 
