da612cf7b9d0d3eb1c7b73f87e7c731eba1bbcca
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-21 23:59:24 +0200

    cleanError dispatch type

--- src/App.js
+++ src/App.js
@@ -2,33 +2,49 @@
 
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
 import { useEffect, useReducer } from "react";
 
 function reducer(state, action) {
   switch (action.type) {
     case "setQuestions":
       return { ...state, questions: action.payload };
+    case "isLoading":
+      return { ...state, isLoading: true };
+    case "isNotLoading":
+      return { ...state, isLoading: false };
+    case "error":
+      return { ...state, error: action.payload };
+    case "cleanError":
+      return { ...state, error: "" };
     default:
       throw new Error("Unknow action.");
   }
 }
 
 export default function App() {
-  const initialState = { questions: [] };
+  const initialState = { questions: [], isLoading: false, error: "" };
+
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { questions } = state;
+  const { questions, isLoading, error } = state;
 
   console.log(questions);
 
   useEffect(function () {
     async function getData() {
       try {
+        dispatch({ type: "cleanError" });
+        dispatch({ type: "isLoading" });
         const res = await fetch("http://localhost:8000/questions");
         if (!res.ok) throw new Error("Something happened.");
         const data = await res.json();
+        if (!data) throw new Error("No data.");
         dispatch({ type: "setQuestions", payload: data });
       } catch (err) {
         console.log(err.message);
+        dispatch({ type: "error", payload: err.message });
+      } finally {
+        dispatch({ type: "isNotLoading" });
       }
     }
     getData();
@@ -39,6 +55,7 @@ export default function App() {
       <Header />
 
       <Main>
+        {isLoading && !error && <Loader />}
         <p>1/15</p>
         <p>Question?</p>
       </Main>
