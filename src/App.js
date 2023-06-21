392f38d25b23675cabc86e9e80eba0576c5f1217
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-21 23:24:05 +0200

    Add server script to package.json, add useEffect to fetch data from fake server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@
 
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect } from "react";
 export default function App() {
+  useEffect(function () {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        if (!res.ok) throw new Error("Something happened.");
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
