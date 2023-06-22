e4809563d87040a24189a6c33d8d9212d5447fe0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-22 10:07:41 +0200

    Create options component to show options inside Question component

--- src/Question.js
+++ src/Question.js
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import Options from "./components/Options";
+
 function Question({ currQuestion }) {
+  const [isCorrect, setIsCorrect] = useState(false);
   const { question, options, correctOption, points } = currQuestion;
-  return <div>{question}</div>;
+
+  function handleAsnwer(i) {
+    i === correctOption ? setIsCorrect(true) : setIsCorrect(false);
+  }
+
+  return (
+    <div>
+      <h4>{question}</h4>
+      <Options options={options} />
+    </div>
+  );
 }
 
 export default Question;
