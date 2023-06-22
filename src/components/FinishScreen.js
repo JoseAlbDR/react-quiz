feee820d07f50952e62c972683b13045b81f5f50
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-22 15:51:25 +0200

    feat: add FinishScreen component and reset button to reset states and show StartScreen component when clicked

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,30 @@
+function FinishScreen({ score, maxScore, dispatch }) {
+  const percentaje = (score / maxScore) * 100;
+  let emoji;
+
+  if (percentaje === 100) emoji = "⭐";
+  if (percentaje < 100 && percentaje >= 80) emoji = "💪";
+  if (percentaje < 80 && percentaje >= 60) emoji = "🎉";
+  if (percentaje < 60 && percentaje >= 40) emoji = "👍";
+  if (percentaje < 40 && percentaje >= 20) emoji = "👌";
+  if (percentaje < 20 && percentaje >= 0) emoji = "🤦‍♂️";
+  if (percentaje === 0) emoji = "🤢";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{score}</strong> out of {maxScore} (
+        {Math.ceil(percentaje)}%)
+      </p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
