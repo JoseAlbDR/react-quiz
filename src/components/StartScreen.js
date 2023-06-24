834dbac2ddcf946690f1b99782b5182f304576ac
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-24 20:59:17 +0200

    Add num questions and difficulty options to StartScreen component

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -5,6 +5,24 @@ function StartScreen({ numQuestions, dispatch }) {
       <h3 className="center">
         {numQuestions} question to test your JavaScript knowledge
       </h3>
+      <div className="start-options">
+        <div>
+          <h4>Num Questions:</h4>
+          <select className="btn">
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={30}>30</option>
+          </select>
+        </div>
+        <div>
+          <h4>Difficulty</h4>
+          <select className="btn">
+            <option value={45}>Easy</option>
+            <option value={30}>Normal</option>
+            <option value={15}>Hard</option>
+          </select>
+        </div>
+      </div>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
