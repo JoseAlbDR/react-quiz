57029fb7653581f8f2c79e5aa20ce1f7ff9b47ce
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-24 20:02:16 +0200

    More questions

--- netlify/functions/data-json.js
+++ netlify/functions/data-json.js
@@ -122,6 +122,84 @@ exports.handler = async (event, context) => {
       The string 'Lydia' is a truthy value. What we're actually asking, is "is this truthy value falsy?". This returns false.`,
     },
     {
+      question: `What's the output?`,
+      code: `let a = 3;
+      let b = new Number(3);
+      let c = 3;
+      
+      console.log(a == b);
+      console.log(a === b);
+      console.log(b === c);`,
+      options: [
+        "true false true",
+        "false false true",
+        "true false false",
+        "false true true",
+      ],
+      correctOption: 2,
+      points: 10,
+      answer: `new Number() is a built-in function constructor. Although it looks like a number, it's not really a number: it has a bunch of extra features and is an object.
+
+      When we use the == operator (Equality operator), it only checks whether it has the same value. They both have the value of 3, so it returns true.
+      
+      However, when we use the === operator (Strict equality operator), both value and type should be the same. It's not: new Number() is not a number, it's an object. Both return false.`,
+    },
+    {
+      question: `What's the output?`,
+      code: `class Chameleon {
+        static colorChange(newColor) {
+          this.newColor = newColor;
+          return this.newColor;
+        }
+      
+        constructor({ newColor = 'green' } = {}) {
+          this.newColor = newColor;
+        }
+      }
+      
+      const freddie = new Chameleon({ newColor: 'purple' });
+      console.log(freddie.colorChange('orange'));`,
+      options: ["orange", "purple", "green", "TypeError"],
+      correctOption: 3,
+      points: 10,
+      answer: `The colorChange function is static. Static methods are designed to live only on the constructor in which they are created, and cannot be passed down to any children or called upon class instances. Since freddie is an instance of class Chameleon, the function cannot be called upon it. A TypeError is thrown.`,
+    },
+    {
+      question: `What's the output?`,
+      code: `let greeting;
+      greetign = {}; // Typo!
+      console.log(greetign);`,
+      options: ["{}", "ReferenceError: greetign is not defined", "undefined"],
+      correctOption: 0,
+      points: 10,
+      answer: `It logs the object, because we just created an empty object on the global object! When we mistyped greeting as greetign, the JS interpreter actually saw this as:
+
+      global.greetign = {} in Node.js
+      window.greetign = {}, frames.greetign = {} and self.greetign in browsers.
+      self.greetign in web workers.
+      globalThis.greetign in all environments.
+      In order to avoid this, we can use "use strict". This makes sure that you have declared a variable before setting it equal to anything.`,
+    },
+    {
+      question: `What happens when we do this?`,
+      code: `function bark() {
+        console.log('Woof!');
+      }
+      
+      bark.animal = 'dog';`,
+      options: [
+        "Nothing, this is totally fine!",
+        "SyntaxError. You cannot add properties to a function this way.",
+        "'Woof' gets logged.",
+        "ReferenceError",
+      ],
+      correctOption: 0,
+      points: 10,
+      answer: `This is possible in JavaScript, because functions are objects! (Everything besides primitive types are objects)
+
+      A function is a special type of object. The code you write yourself isn't the actual function. The function is an object with properties. This property is invocable.`,
+    },
+    {
       question: ``,
       code: ``,
       options: ["", "", "", ""],
